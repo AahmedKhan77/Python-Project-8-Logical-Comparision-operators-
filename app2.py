a247c02526889578b3334a4eb17a02d840c798e3
Aahmed Khan <khanaahmed272@gmail.com>
2024-07-28 00:31:24 +0500

    Learned about logical and conditional operators

---
+++ app2.py
@@ -0,0 +1,32 @@
+temperature = 30
+
+if temperature > 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
+if temperature >= 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
+if temperature < 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
+if temperature >= 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
+if temperature == 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
+if temperature != 30:
+    print("its a hot day")
+else:
+    print("its not a hot day")
+
---
+++ app3.py
@@ -0,0 +1,8 @@
+name = "k2l"
+
+if len(name) > 50:
+    print("name can be max 50 characters")
+elif len(name) < 3:
+    print("Name can be minimum 3 characters long")
+else:
+    print("Name is OK")
